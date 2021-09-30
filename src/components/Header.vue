8b6dc78a93caa13e62558a465af9f86781228101
Roberto <robertomartinonwg@gmail.com>
2021-09-30 15:11:46 +0200

    struttura Header

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="containerHeader">
+      <div class="logo">
+        <!-- LOGO -->
+        <h1>pippo</h1>
+      </div>
+      <div class="link">
+        <h1>poppo</h1>
+        <!-- <li> -->
+      </div>
+    </div>
+      
+   
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../style/variables';
+
+.containerHeader {
+  width: 100%;
+  height: 190px;
+  display: flex;
+
+    .logo {
+      width: 50%;
+      height: 100%;
+      background-color: azure;
+        
+    }
+    .link {
+      width: 50%;
+      height: 100%;
+      background-color: lightgreen;
+    }
+}
+</style>
