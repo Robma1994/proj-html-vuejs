c9233237dff1f3984ebe2b9effd1e23e7dd31f7c
Roberto <robertomartinonwg@gmail.com>
2021-10-01 19:06:32 +0200

    Section Form - Section activity

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
       </div>
     </div>
 <!-- Blocco due - World Class Fitness -->
-    <div class="container height300">
+    <div class="container height300 backgroundLightGray">
       <div class="container-small height100 verOriCentering">
         <div class="verOriCentering dirCol">
-          <h2>World Class Fitness</h2>
-          <h3>Ciao pippo</h3>
+          <h2 class="marginB10">World Class Fitness</h2>
+          <img class="marginB10" src="./assets/divider-x-red.png" alt="">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odit in itaque reprehenderit. Voluptatibus animi deus sed?</p>
         </div>
       </div>
@@ -25,18 +25,38 @@
     <div class="container height350">
       <div class="container-small height100">
         <!-- Inserendo contenuto posso eliminare le altezze fisse e rendere il tutto dinamico -->
-        
         <div class="verOriCentering height350"> <!-- Utilizzando flex con un div mancante, avrei perso elasticità-->
           <div class="height200 width300 marginR10">
-            <!-- immagine -->
+            <img src="./assets/home_img-compressor.jpg" alt="">
           </div>
           <div class="height200 width300 marginL10">
-            <!-- Form -->
+            <h3 class="marginB10 font-s18 colorWhite">Sign Up For A Free Training Sessione!</h3>
+            <form class="orizzontal dirCol">
+              <label for="name">Name</label>
+              <input type="text" name="name" placeholder="Scrivi il tuo nome">
+              <label for="e-mail">E-mail</label>
+              <input type="text" email="e-mail" placeholder="Scrivi la tua e-mail">
+              <input type="submit" value="START NOW">
+            </form>
           </div>
         </div>
       </div>
     </div>
 <!-- Fine Blocco Tre - Form -->  
+<!-- Activities -->
+    <div class="backActivities height350">
+      <div class="container-small height100">
+        <div class="wrapper-Activities" v-for="(activ,index) in activity" :key="index"> 
+          <div class="cycleIcon verOriCentering marginB10">
+            <i class="largeIcon" :class="`fas ${activ.icon}`"></i>
+          </div>
+          <h3 class="marginB10">{{activ.title}}</h3>
+          <img class="marginB10" src="./assets/divider-x-red.png" alt="">
+          <p class="marginB10">{{activ.text}}</p>
+        </div>
+      </div>
+    </div>
+<!-- Fine Activities -->
     <div class="container backgroundShark">
       <div class="container-small wrapper">
         <FooterTop />
@@ -54,13 +74,18 @@
 import Header from './components/Header.vue'
 import FooterTop from './components/FooterTop.vue'
 import FooterBottom from './components/FooterBottom.vue'
-
+import activities from './assets/dati/activities.js'
 export default {
   name: 'App',
   components: {
     Header,
     FooterTop,
     FooterBottom
+  },
+  data() {
+    return {
+      activity: activities
+    }
   }
 }
 </script>
@@ -68,7 +93,7 @@ export default {
 <style lang="scss">
 @import './style/generals';
 @import './style/variables';
-
+//SEZIONE HEADER
 .background {
   background-image: url('assets/home-banner.jpg');
   background-size: cover;
@@ -76,10 +101,7 @@ export default {
 .container {
   width: 100%;
 }
-.container-small {
-  //background-color: rgba($color: blue, $alpha: 0.3);
-  //border: 1px solid black;
-}
+
 .title {
   font-size: $fontSize65;
   color: $colorWhite;
@@ -102,10 +124,75 @@ export default {
       color: gray;
     }
 }
+
+.container {
+  .container-small{
+    .dirCol {
+        h2 {
+          color: $colorWhite;
+          font-size: $fontSize30;
+        }
+        p {
+          color: $colorDoveGray;
+        }
+    }
+  }
+}
+//SEZIONE WORLD
+.container {
+  background-color: $backgroundDarkGray;
+  .container-small {
+    .verOriCentering {
+      .height200 {
+
+          img {
+            width: 100%;
+          }
+      }
+    }
+  }
+}
+//SEZIONE FORM
+//FINE SEZIONE FORM
+
+//SEZIONE ACTIVITIES
+.backActivities {
+  background-image: url('./assets/gym_bkgd_bw-compressor.jpg');
+  background-position: center;
+  .container-small {
+    display: flex;
+    .wrapper-Activities {
+      //width: calc(100% / 4);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+        .cycleIcon {
+          width: 60px;
+          height: 60px;
+          border: 1px solid red;
+          border-radius: 50%;
+
+            .largeIcon{
+              font-size: $fontSize30;
+            }
+        }
+        p {
+          text-align: center;
+        }
+    }
+  }
+}
+//FINE SEZIONE ACTIVITIES
+
 .backgroundShark {
     background-color: $backgroundShark;
 }
 .backgroundLightBlack {
   background-color: $colorLightBlack;
 }
+.backgroundLightGray {
+  background-color: $backgroundGrayLight;
+}
+
 </style>
---
+++ src/assets/dati/activities.js
@@ -0,0 +1,24 @@
+export default 
+[
+    {
+        icon: 'fa-bicycle',
+        title: 'Streng & Condition',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+    {
+        icon: 'fa-bicycle',
+        title: 'Fitness & Cardio',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+    {
+        icon: 'fa-bicycle',
+        title: 'Flexibility & Rest',
+      
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+    {
+        icon: 'fa-bicycle',
+        title: 'Healt & Diet',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+]
